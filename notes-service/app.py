1f6156a0eec7cb0aac32d72c0ab2d1b604b7bcc9
uadeel421 <127033876+uadeel421@users.noreply.github.com>
2025-04-09 22:13:36 +0500

    Update app.py

--- notes-service/app.py
+++ notes-service/app.py
@@ -110,7 +110,9 @@ async def delete_note(note_id: int, db: Session = Depends(get_db), current_user:
     db_note = db.query(Note).filter(Note.id == note_id,
                                     Note.owner_id == current_user["id"]).first()
     if not db_note:
-        raise HTTPException(status_code=404, detail="Note not found")
+        raise HTTPException(status_code=404, 
+                            detail="Note not found")
+   
     try:
         db.delete(db_note)
         db.commit()
